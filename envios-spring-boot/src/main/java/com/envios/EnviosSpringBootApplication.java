ca4451452f4e5f6ad85d201aa2c84e439cfb4629
NicolasOlivares22 <ni.olivaresm@duocuc.cl>
2025-06-08 23:37:50 -0400

    api envios

---
+++ envios-spring-boot/src/main/java/com/envios/EnviosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.envios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviosSpringBootApplication.class, args);
+	}
+
+}
