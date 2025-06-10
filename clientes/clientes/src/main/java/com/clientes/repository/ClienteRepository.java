91a98df29be715b8f293ebb04c7d33556e8edb0d
NicolasOlivares22 <ni.olivaresm@duocuc.cl>
2025-06-10 16:05:07 -0400

    Delete clientes/clientes directory

--- clientes/clientes/src/main/java/com/clientes/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clientes.repository;
-
-import com.clientes.models.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
